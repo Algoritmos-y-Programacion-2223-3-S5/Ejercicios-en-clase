4035ec3886b792bbc05f35329c9a9990be9ea289
aguerraUnimet <aguerra@unimet.edu.ve>
2023-04-24 08:32:24 -0400

    :sparkles: Clase de variables

---
+++ Semana 2/Lunes/ejercicio1.py
@@ -0,0 +1,5 @@
+peso = input("Por favor ingresa tu peso: ")
+edad = input("Por favor ingresa tu edad: ")
+
+print("Tu peso es de: "+peso+" kg")
+print("Tu edad es: "+edad)
---
+++ Semana 2/Lunes/ejercicio2.py
@@ -0,0 +1,6 @@
+base = input("Por favor ingrese la base: ")
+altura = input("Por favor ingrese la altura: ")
+base = float(base)
+altura = float(altura)
+resultado = (base * altura) / 2
+print("El area del triangulo es",resultado)
